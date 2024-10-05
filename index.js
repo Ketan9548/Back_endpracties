c434c5267cd9202b4334f503f65bf86902135a2a
kapil <kc918062@gmail.com>
2024-10-05 22:03:48 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.get('/', (req, res) => {
+    res.send('hellow kapil')
+})
+app.get('/amazon', (req, res) => {
+    res.send('your order is picked')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log("congratulationo you made a server")
+    console.log(`this is your back-end port ${port}`)
+})

4ea010aff02afa4ef910f20946f7e5fa6d383558
Ketan9548 <kc918062@gmail.com>
2024-10-22 10:50:43 +0530

    done

---
+++ realtimebackendproject/src/utils/asynchandler.js
@@ -0,0 +1,8 @@
+// higher order function exaple
+// const asynHandler = () => {}
+// const asynHandler = (fn) => {() => {}}
+// const asynHandler = (fn) => async () => {}
+
+const asynHandler = (fun) =>()=>{
+
+}
